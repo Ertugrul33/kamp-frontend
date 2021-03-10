c92ce4c4128b973656422cbda77b13d3f06eb679
Ertuğrul EŞOL <ertugrul.esol@gmail.com>
2021-03-11 00:06:56 +0300

    10 Mart 2021 Ders Sonu

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
-import { HttpClient } from '@angular/common/http';
 import { ProductResponseModel } from 'src/app/models/productResponseModel';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product',
@@ -10,21 +10,25 @@ import { ProductResponseModel } from 'src/app/models/productResponseModel';
 })
 export class ProductComponent implements OnInit {
   products: Product[] = [];
-  apiUrl = 'https://localhost:44301/api/products/getall';
+  dataLoaded = false;
 
   //private sadece bu sınıfta çalışacak demektir.
   //JavaScript'te her şey class değil, fonksiyondur.
-  constructor(private httpClient: HttpClient) {}
+  constructor(private productService:ProductService) {}
 
   ngOnInit(): void {
     this.getProducts();
   }
 
   getProducts() {
-    this.httpClient
-      .get<ProductResponseModel>(this.apiUrl)
-      .subscribe((response) => {
-        this.products = response.data
-      });
+    //console.log("Api request başladı.")
+    //subsribe asenkron çalışır.
+    this.productService.getProducts().subscribe(response=>{
+      this.products = response.data
+      this.dataLoaded = true;
+      //console.log("Api request bitti.");
+    })
+    //console.log("Metod bitti.");
+    
   }
 }
