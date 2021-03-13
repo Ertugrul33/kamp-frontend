b0fe3df4ffc6af65e5d779bc3d81e4497765d044
Ertuğrul EŞOL <ertugrul.esol@gmail.com>
2021-03-13 22:56:41 +0300

    13 Mart 2021 Commit 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductComponent } from './components/product/product.component';
 
 const routes: Routes = [
-  {path:"",pathMatch:"full", component:ProductComponent},
-  {path:"products", component:ProductComponent}
+  { path: '', pathMatch: 'full', component: ProductComponent },
+  { path: 'products', component: ProductComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
